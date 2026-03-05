1e91020f600d2cbc5be8de6ead9ad7a1fe1251a7
Yashkalamkar23 <chmcs2026.yash.kalamkar@gmail.com>
2026-03-05 11:29:24 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const api = axios.create({
-    // baseURL : 'https://mern-bookstore-lerh.onrender.com'
-    baseURL : 'http://localhost:3000'
+     baseURL : 'https://tyre-store-management-system.onrender.com'
+    // baseURL : 'http://localhost:3000'
 })
 export default api
